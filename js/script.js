1e3e74d824d0b08aa1f3506c809227b27486288e
Lorena Sánchez <lorena.sanchez@alumnos.udit.es>
2025-11-19 00:01:08 +0100

    img_lata_home
    Imagenes de latas y sus descripciones y links

--- js/script.js
+++ js/script.js
@@ -128,12 +128,12 @@ $(document).ready(function(){
   
     // Array de productos (puedes añadir artista, estilo, etc.)
     const products = [
-      {id:"p1", title:"Impresión, sol naciente", price:9.5, img:"https://picsum.photos/600/600?random=41", artist:"Claude Monet", style:"impresionismo"},
-      {id:"p2", title:"Baile en el Moulin Rouge", price:11, img:"https://picsum.photos/600/600?random=42", artist:"Toulouse-Lautrec", style:"impresionismo"},
-      {id:"p3", title:"Los girasoles", price:10, img:"https://picsum.photos/600/600?random=43", artist:"Vincent Van Gogh", style:"impresionismo"},
-      {id:"p4", title:"Nenúfares", price:12, img:"https://picsum.photos/600/600?random=44", artist:"Claude Monet", style:"impresionismo"},
-      {id:"p5", title:"La noche estrellada", price:9.8, img:"https://picsum.photos/600/600?random=45", artist:"Vincent Van Gogh", style:"impresionismo"},
-      {id:"p6", title:"El almendro en flor", price:12, img:"https://picsum.photos/600/600?random=45", artist:"Vincent Van Gogh", style:"impresionismo"}
+      {id:"p1", title:"Impresión, sol naciente", price:9.5, img:"../img/LATA_NARANJA.jpg", artist:"Claude Monet", style:"impresionismo"},
+      {id:"p2", title:"Baile en el Moulin Rouge", price:11, img:"../img/LATA_ROSA.jpg", artist:"Toulouse-Lautrec", style:"impresionismo"},
+      {id:"p3", title:"Los girasoles", price:10, img:"../img/LATA_AMARILLA.jpg", artist:"Vincent Van Gogh", style:"impresionismo"},
+      {id:"p4", title:"Nenúfares", price:12, img:"../img/LATA_MORADA.jpg", artist:"Claude Monet", style:"impresionismo"},
+      {id:"p5", title:"La noche estrellada", price:9.8, img:"../img/LATA_AZUL.jpg", artist:"Vincent Van Gogh", style:"impresionismo"},
+      {id:"p6", title:"El almendro en flor", price:12, img:"../img/LATA_VERDE.jpg", artist:"Vincent Van Gogh", style:"impresionismo"}
       // Añadir más según necesites
     ];
   
